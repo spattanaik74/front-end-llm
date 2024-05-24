a9b8e75eec1bd167e0b6ee8e52a3669b77fef4ad
Nasib <nasib4patel@gmail.com>
2024-05-24 21:43:10 +0530

    first  message

--- src/lib/data.js
+++
@@ -1,6 +0,0 @@
-export const team = [
-    { id: 1, name: 'Alice', role: 'Developer' },
-    { id: 2, name: 'Bob', role: 'Designer' },
-    { id: 3, name: 'Charlie', role: 'Product Manager' }
-  ];
-  
